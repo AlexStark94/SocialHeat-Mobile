633ef6f65027e40acbf2c1706515de00c8315dff
alex-ridian <alex@ridian.io>
2023-10-05 21:15:12 -0300

    Heat maps with mocked data on home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,60 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-
+import { Dimensions, StyleSheet } from 'react-native';
 import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler';
+import MapView, { Heatmap, PROVIDER_GOOGLE } from 'react-native-maps';
+
+const screenHeight = Dimensions.get('window').height;
 
 export default function HomeScreen() {
+  const points = [
+    {latitude: 37.78832, longitude: -122.4327, weight: 1},
+    {latitude: 37.78759, longitude: -122.4295, weight: 0.5},
+    {latitude: 37.78535, longitude: -122.4446, weight: 0.2},
+    {latitude: 37.78505, longitude: -122.4400, weight: 0.1},
+    {latitude: 37.78450, longitude: -122.4350, weight: 0.05},
+    {latitude: 37.78410, longitude: -122.4310, weight: 0.01},
+    {latitude: 37.78410, longitude: -122.4120, weight: 0.01},
+    {latitude: 37.78410, longitude: -122.4130, weight: 0.05},
+    {latitude: 37.78010, longitude: -122.4330, weight: 0.05},
+    {latitude: 37.77010, longitude: -122.4330, weight: 0.05},
+    {latitude: 37.77000, longitude: -122.4320, weight: 0.01},
+    {latitude: 37.74535, longitude: -122.3446, weight: 0.5},
+    {latitude: 37.73535, longitude: -122.2446, weight: 0.01},
+    {latitude: 37.63535, longitude: -122.1446, weight: 0.2},
+    {latitude: 37.63535, longitude: -121.4446, weight: 0.9}
+  ];
+  
   return (
     <GestureHandlerRootView>
       <ScrollView style={styles.container}>
-        
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Heatmap
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            points={points}
+            radius={40}
+            gradient={{
+              colors: ["yellow", "orange", "red", "purple"],
+              startPoints: [0.01, 0.1, 0.65, 0.9],
+              colorMapSize: 2000
+            }}
+          >
+
+          </Heatmap>
+        </MapView>
       </ScrollView>
     </GestureHandlerRootView>
   );
@@ -16,8 +63,11 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#FFFFFF',
-    paddingHorizontal: 16,
     marginTop: 16,
-    flex: 1,
+    flexGrow: 1,
+  },
+  map: {
+    height: screenHeight,
+    width: '100%',
   },
 });
